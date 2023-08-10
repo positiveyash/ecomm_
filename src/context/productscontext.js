86925130e69424fe76faae2ab84167b4bab14784
Yash Bhatt <bhattyash15@gmail.com>
2023-08-10 14:36:16 +0530

    new file added

--- src/context/productscontext.js
+++ src/context/productscontext.js
@@ -1,1 +1,20 @@
-import {createcontext} from "react";
+import {createcontext,useContext} from "react";
+
+const AppContext=createcontext();
+
+const AppProvider=({children})=>{
+    return (
+       
+<AppContext.Provider value={{myName:"yash"}}>
+{children}
+</AppContext.Provider>
+    );
+};
+
+const useProductContext=()=>{
+    return useContext(AppContext);
+};
+
+export {AppProvider,AppContext,useProductContext};
+
+
